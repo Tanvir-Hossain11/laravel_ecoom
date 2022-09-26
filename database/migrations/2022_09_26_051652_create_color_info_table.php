6b304d954df8cd90fa12d9c7420fba14b9bd8cd5
Tanvir Hossain <hossaintanvirsub@gmail.com>
2022-09-26 11:46:36 +0600

    upadtae

--- database/migrations/2022_09_26_051652_create_color_info_table.php
+++ database/migrations/2022_09_26_051652_create_color_info_table.php
@@ -13,10 +13,11 @@ return new class extends Migration
      */
     public function up()
     {
+        //  Schema::dropIfExists('color_info');
         Schema::create('color_info', function (Blueprint $table) {
             $table->id();
-            $table->string('name',200);
-            
+            $table->string('name', 200);
+
             $table->string('product_id');
             $table->string('product_category');
             $table->double('amount', 8, 2);
